de08c161c23ce5cef83d823b1ce0441c5c230e22
Samir Chowdhury <samirchowd@gmail.com>
2021-10-29 13:35:12 -0400

    Finished edit feature. Need to exclude !commandsfrom log

--- src/app/app.py
+++ src/app/app.py
@@ -1,4 +1,4 @@
-
+from pprint import pprint as pp
 class conversation():
     def __init__(self, num_utters = 50):
         self.conv = {}
@@ -7,6 +7,7 @@ class conversation():
 
     def listen(self):
         inp = input()
+
         self.conv[self.id]= {'id': self.id,
                              'message': inp,
                              'author': 'user',
@@ -15,6 +16,9 @@ class conversation():
         return inp
 
     def respond(self, inp):
+        if inp.startswith('!'):
+            return None
+
         self.conv[self.id] = {'id': self.id,
                               'message': inp,
                               'author': 'model',
@@ -23,10 +27,28 @@ class conversation():
         self.id += 1
         return inp
 
-    def tag(self, tags):
-        pass
+    def tag(self):
+        # Demarking Tagging Start
+        print('='*30, end='\n')
+
+        # Asking User
+        tags = input('Please input the tags for the previous statement in a comma seperated list\n')
+        self.conv[self.id-1]['tags'] = tags.split(',')
+
+        # Confirming tags
+        print('The following tags have been recorded: ', self.conv[self.id-1]['tags'])
+
+        # Demarking Tagging End
+        print('='*30, end='\n')
 
     def edit(self):
+        print('='*30)
+        pp(self.conv)
+        edit_id = int(input('Please select an utterance id to edit: '))
+        old_tags = self.conv[edit_id]['tags']
+        tags = input('Please enter new tags in a comma seperated list: ').split(',')
+        self.conv[edit_id]['tags'] = tags
+        print(f'Replaced tags for ID{edit_id}: {old_tags} -> {tags}')
         print('='*30)
 
     def run(self):
@@ -38,8 +60,8 @@ class conversation():
                 self.edit()
             elif inp == '!tag':
                 self.tag()
-            else:
-                response = self.respond(str(inp))
+            response = self.respond(str(inp))
+            if response:
                 print(response, end='\n')
             inp = self.listen()
 
@@ -49,4 +71,4 @@ class conversation():
 if __name__ == '__main__':
     conv = conversation()
     conv.run()
-    print(conv.conv)
+    pp(conv.conv)
